53050cecc30de4d31d2ce8bd97de38e8dfdd21d2
lightcyan area51 <lightcyan.area51@gmail.com>
2012-11-21 00:44:44 +0900

    Add five files.
    sb-cri.el
    sb-epoch.el
    sb-ryukyu.el
    sb-taiwantoday.el
    sb-taiwantoday2.el

---
+++ sb-ryukyu.el
@@ -0,0 +1,365 @@
+;;; sb-ryukyu.el --- shimbun backend for ryukyushimpo.jp -*- coding: iso-2022-7bit; -*-
+
+;; Modified from sb-yomiuri.el by lightcyan <lightcyan.area51@gmail.com>
+;; URL: http://d.hatena.ne.jp/lightcyan/
+;; Below is the original copyright notice of sb-reuters-rss.el.
+
+;; Copyright (C) 2001, 2002, 2003, 2004, 2005 Authors
+
+;; Author: TSUCHIYA Masatoshi <tsuchiya@namazu.org>,
+;;         Yuuichi Teranishi  <teranisi@gohome.org>,
+;;         Katsumi Yamaoka    <yamaoka@jpl.org>
+
+;; Keywords: news
+
+;;; Copyright:
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 2, or (at your option)
+;; any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program; if not, you can either send email to this
+;; program's maintainer or write to: The Free Software Foundation,
+;; Inc.; 59 Temple Place, Suite 330; Boston, MA 02111-1307, USA.
+
+;;; Commentary:
+
+;; Original code was nnshimbun.el written by
+;; TSUCHIYA Masatoshi <tsuchiya@namazu.org>.
+
+;;; Code:
+
+(require 'shimbun)
+(require 'sb-text)
+
+(luna-define-class shimbun-ryukyu
+		   (shimbun-japanese-newspaper shimbun-text) ())
+
+(defvar shimbun-ryukyu-top-level-domain "ryukyushimpo.jp"
+  "Name of the top level domain for the 琉球新報.")
+
+(defvar shimbun-ryukyu-url
+  (concat "http://" shimbun-ryukyu-top-level-domain "/")
+  "Name of the parent url.")
+
+(defvar shimbun-ryukyu-group-table
+  (let* ((s0 "[\t\n ]*")
+	 (s1 "[\t\n ]+")
+	 (default
+	   (list
+	    (concat
+	     "<a" s1 "href=\""
+	     ;; 1. url
+	     "\\(http://[^/]+/news/"
+	     ;; 2. serial
+	     "storyid-\\([0-9a-z]+-storytopic-[0-9a-z]+\\)[^\"]+\\)\">"
+	     ;; 3. subject
+	     "\\([^<]+\\)</a>"
+	     ;; 4. year
+	     "(\\(20[0-9][0-9]\\)\\."
+	     ;; 5. month
+	     "\\([01][0-9]\\)\\."
+	     ;; 6. date
+	     "\\([0-3][0-9]\\))")
+	    1 2 2 4 3 5 6)))
+    `(("society" "社会" "storytopic-1.html" ,@default)
+      ("politics" "政治" "storytopic-3.html" ,@default)
+      ("economy" "経済" "storytopic-4.html" ,@default)
+      ("region" "地域" "storytopic-5.html" ,@default)
+      ("education" "教育" "storytopic-7.html" ,@default)
+      ("ent" "エンタメ" "storytopic-137.html" ,@default)
+      ("editorial" "社説" "storytopic-11.html" ,@default)
+      ("column" "コラム" "storytopic-12.html" ,@default)
+      ))
+"Alist of group names, their Japanese translations, index pages,
+regexps and numbers.
+Regexp may contain the \"%s\" token which is replaced with a
+regexp-quoted group name.  Numbers point to the search result in order
+of [0]url, [1,2]serial numbers, [3]year, [4]subject, [5]month, [6]day,
+\[7]hour:minute, [8]ja month, [9]ja day and [10]subgroup.")
+
+(defvar shimbun-ryukyu-content-start
+  "\n<!--// article_start //-->\n\\|\n<!--  honbun start  -->\n")
+
+(defvar shimbun-ryukyu-content-end
+  "\n<!--// article_end //-->\n\\|\n<!--  honbun end  -->\n")
+
+(defvar shimbun-ryukyu-x-face-alist
+  '(("default" . "X-Face: #sUhc'&(fVr$~<rt#?PkH,u-.fV(>y)\
+i\"#,TNF|j.dEh2dAzfa4=IH&llI]S<-\"dznMW2_j\n [N1a%n{SU&E&\
+Ex;xlc)9`]D07rPEsbgyjP@\"_@g-kw!~TJNilrSC!<D|<m=%Uf2:eebg")))
+
+(defvar shimbun-ryukyu-expiration-days 7)
+
+(luna-define-method initialize-instance :after ((shimbun shimbun-ryukyu)
+						 &rest init-args)
+  (shimbun-set-server-name-internal shimbun "琉球新報")
+  (shimbun-set-from-address-internal shimbun "nobody@example.com")
+  ;; To share class variables between `shimbun-ryukyu' and its
+  ;; successor `shimbun-ryukyu-html'.
+  (shimbun-set-x-face-alist-internal shimbun shimbun-ryukyu-x-face-alist)
+  (shimbun-set-expiration-days-internal shimbun
+					shimbun-ryukyu-expiration-days)
+  shimbun)
+
+(luna-define-method shimbun-groups ((shimbun shimbun-ryukyu))
+  (mapcar 'car shimbun-ryukyu-group-table))
+
+(luna-define-method shimbun-current-group-name ((shimbun shimbun-ryukyu))
+  (nth 1 (assoc (shimbun-current-group-internal shimbun)
+		shimbun-ryukyu-group-table)))
+
+(luna-define-method shimbun-index-url ((shimbun shimbun-ryukyu))
+  (let* ((group (shimbun-current-group-internal shimbun))
+	 (index (nth 2 (assoc group shimbun-ryukyu-group-table))))
+    (if (string-match "\\`http:" index)
+	index
+      (concat shimbun-ryukyu-url "news/" index))))
+
+;; (defun shimbun-ryukyu-japanese-string-to-number (string)
+;;   "Convert a Japanese zenkaku number to just a number."
+;;   (let ((alist '((?０ . 0) (?１ . 1) (?２ . 2) (?３ . 3) (?４ . 4)
+;; 		 (?５ . 5) (?６ . 6) (?７ . 7) (?８ . 8) (?９ . 9)))
+;; 	(len (length string))
+;; 	(idx 0)
+;; 	(num 0))
+;;     (while (< idx len)
+;;       (setq num (+ (cdr (assq (aref string idx) alist)) (* num 10))
+;; 	    idx (1+ idx)))
+;;     num))
+
+(defun shimbun-ryukyu-get-top-header (group from)
+  "Return a list of a header for the top news."
+  (when (re-search-forward
+	 (format
+	  (eval-when-compile
+	    (let ((s0 "[\t\n ]*")
+		  (s1 "[\t\n ]+"))
+	      (concat
+	       "<a" s1 "href=\"/"
+	       ;; 1. url
+	       "\\("
+	       ;; 2. subgroup
+	       "\\(?:\\([^/]+\\)/\\)?"
+	       "news/"
+	       ;; 3. serial number[1]
+	       "\\("
+	       ;; 4. year
+	       "\\(20[0-9][0-9]\\)"
+	       "[01][0-9][0-3][0-9]\\)"
+	       ;; 5. serial number[2]
+	       "\\([^.]+\\)"
+	       "[^\"]+\\)"
+	       "\"[^>]*>" s0
+	       ;; 6. subject
+	       "\\([^<]+\\)"
+	       s0)))
+	  group)
+	 nil t)
+    (let* ((url (shimbun-expand-url (match-string 1) shimbun-ryukyu-url))
+	   (subgroup (match-string 2))
+	   (id (concat "<" (match-string 3) "." (match-string 5)
+		       "@" (when subgroup
+			     (concat subgroup "."))
+		       group "." shimbun-ryukyu-top-level-domain ">"))
+	   (year (string-to-number (match-string 4)))
+	   (subject (if subgroup
+			(concat "[" subgroup "] " (match-string 6))
+		      (match-string 6))))
+      (prog1
+	  (when (re-search-forward
+		 (eval-when-compile
+		   (let ((s0 "[\t\n ]*"))
+		     (concat
+		      ">" s0 "（" s0
+		      ;; 1. month
+		      "\\([01]?[0-9]\\)"
+		      s0 "月" s0
+		      ;; 2. day
+		      "\\([0-3][0-9]\\)"
+		      s0 "日" s0
+		      ;; 3. hour:minute
+		      "\\([012][0-9]:[0-5][0-9]\\)"
+		      s0 "）" s0 "<")))
+		 nil t)
+	    (list (shimbun-create-header
+		   0
+		   subject
+		   from
+		   (shimbun-make-date-string
+		    year
+		    (string-to-number (match-string 1))
+		    (string-to-number (match-string 2))
+		    (match-string 3))
+		   id "" 0 0 url)))
+	(goto-char (point-min))))))
+
+(defun shimbun-ryukyu-get-headers (shimbun)
+  "Return a list of headers."
+  (shimbun-strip-cr)
+  (goto-char (point-min))
+  (let ((group (shimbun-current-group-internal shimbun))
+	(from (concat (shimbun-server-name shimbun)
+		      " (" (shimbun-current-group-name shimbun) ")"))
+	(case-fold-search t)
+	headers regexp numbers subject month day subgroup)
+    ;; Extract top news.
+    (when (member group '("atmoney" "entertainment" "national" "politics"
+			  "science" "sports" "world"))
+      (setq headers (shimbun-ryukyu-get-top-header group from)))
+    (setq regexp (assoc group shimbun-ryukyu-group-table)
+	  numbers (nthcdr 4 regexp)
+	  regexp (format (nth 3 regexp) (regexp-quote group)))
+    (while (re-search-forward regexp nil t)
+      (setq subject (match-string (nth 4 numbers))
+	    month (string-to-number (match-string (nth 5 numbers)))
+	    day (string-to-number (match-string (nth 6 numbers)))
+	    subgroup (when (nth 10 numbers)
+		       (match-string (nth 10 numbers))))
+      (cond ((string-equal group "editorial")
+	     (setq subject
+		   (format
+		    "%02d/%02d %s"
+		    month day
+		    (save-match-data
+		      (if (string-match "\\`［\\(.+\\)］「\\(.+\\)」\\'"
+					subject)
+			  (replace-match "\\1: \\2" nil nil subject)
+			subject)))))
+	    (subgroup
+	     (setq subject (concat "[" subgroup "] " subject))))
+      (push (shimbun-create-header
+	     0 subject from
+	     (shimbun-make-date-string
+	      (string-to-number (match-string (nth 3 numbers)))
+	      month day
+	      (when (nth 7 numbers)
+		(match-string (nth 7 numbers))))
+	     (concat "<" (match-string (nth 1 numbers))
+		     "." (match-string (nth 2 numbers))
+		     "%" (when subgroup
+			   (concat subgroup "."))
+		     group "." shimbun-ryukyu-top-level-domain ">")
+	     "" 0 0
+	     (shimbun-expand-url (match-string (nth 0 numbers))
+				 shimbun-ryukyu-url))
+	    headers))
+    (shimbun-sort-headers headers)))
+
+(luna-define-method shimbun-get-headers ((shimbun shimbun-ryukyu)
+					 &optional range)
+  (shimbun-ryukyu-get-headers shimbun))
+
+(defun shimbun-ryukyu-prepare-article (shimbun header)
+  (shimbun-strip-cr)
+  (goto-char (point-min))
+  (let ((case-fold-search t)
+	start)
+    (when (and (re-search-forward shimbun-ryukyu-content-start nil t)
+	       (progn
+		 (setq start (match-end 0))
+		 (re-search-forward shimbun-ryukyu-content-end nil t)))
+      (narrow-to-region start (match-beginning 0))
+      (goto-char (point-min))
+      ;; Remove trailing whitespace.
+      (while (re-search-forward "[\t ]+$" nil t)
+	(delete-region (match-beginning 0) (match-end 0)))
+      (goto-char (point-min))
+      ;; Correct Date header.
+      (when (re-search-forward
+	     (eval-when-compile
+	       (let ((s0 "[\t\n ]*")
+		     (s1 "[\t\n ]+"))
+		 (concat
+		  "<!--" s0 "//" s1 "date_start" s1 "//" s0 "-->" s0
+		  ;; 1. year
+		  "\\(20[0-9][0-9]\\)"
+		  s0 "年" s0
+		  ;; 2. month
+		  "\\([01]?[0-9]\\)"
+		  s0 "月" s0
+		  ;; 3. day
+		  "\\([0-3]?[0-9]\\)"
+		  s0 "日" s0
+		  ;; 4. hour
+		  "\\([012]?[0-9]\\)"
+		  s0 "時" s0
+		  ;; 5. minute
+		  "\\([0-5]?[0-9]\\)"
+		  s0 "分" s0 "<!--" s0 "//" s1 "date_end" s1 "//" s0 "-->")))
+	     nil t)
+	(shimbun-header-set-date
+	 header
+	 (shimbun-make-date-string
+	  (string-to-number (match-string 1))
+	  (string-to-number (match-string 2))
+	  (string-to-number (match-string 3))
+	  (format "%02d:%02d"
+		  (string-to-number (match-string 4))
+		  (string-to-number (match-string 5)))))
+	(goto-char (point-min)))
+      ;; Remove the フォトニュース, the 写真の拡大 buttons, etc.
+      (while (re-search-forward
+	      (eval-when-compile
+		(let ((s0 "[\t\n ]*")
+		      (s1 "[\t\n ]+")
+		      (n1 "[^\t\n >]+"))
+		  (concat
+		   s0
+		   "\\(?:"
+		   "<a\\(?:" s1 n1 "\\)*" s1
+		   "\\(?:class=\"photo-pn\"\\|target=\"photoWin\"\\)"
+		   "\\(?:" s1 n1 "\\)*" s0 ">"
+		   "\\|"
+		   "<img\\(?:" s1 n1 "\\)*" s1
+		   "\\(?:alt=\"フォトニュース\"\\|class=\"photo-el\"\\)"
+		   "\\(?:" s1 n1 "\\)*" s0 ">"
+		   "\\|"
+		   "<div" s1
+		   "class=\"enlargedphoto\">\\(?:[^<>]+\\)?"
+		   "<img" s1 "[^>]+>" s0 "</div>"
+		   "\\|"
+		   "<div" s1 "class=\"[^\"]+\">" s0
+		   "<img\\(?:" s1 n1 "\\)*" s1 "src=\"/g/d\\.gif\""
+		   "\\(?:" s1 n1 "\\)*" s0 ">" s0 "</div>"
+		   "\\|"
+		   s0 "rectangle(\"[^\"]+\");" s0
+		   "\\)" s0)))
+	      nil t)
+	(delete-region (match-beginning 0) (match-end 0)))
+      (goto-char (point-min))
+      ;; Replace 写真の拡大 with 写真.
+      (while (re-search-forward
+	      (eval-when-compile
+		(let ((s1 "[\t\n ]+")
+		      (n1 "[^\t\n >]+"))
+		  (concat "<img\\(?:" s1 n1 "\\)*" s1
+			  "alt=\"写真\\(の拡大\\)\"")))
+	      nil t)
+	(delete-region (match-beginning 1) (match-end 1)))
+      (goto-char (point-min))
+      ;; Break continuous lines in editorial articles.
+      (when (and (string-equal "editorial"
+			       (shimbun-current-group-internal shimbun))
+		 (string-match " \\(?:よみうり寸評\\|編集手帳\\)\\'"
+			       (shimbun-header-subject header 'no-encode)))
+	(goto-char (point-min))
+	(while (search-forward "◆" nil t)
+	  (replace-match "。<br><br>　")))
+      (widen)))
+  (goto-char (point-min)))
+
+(luna-define-method shimbun-make-contents :before ((shimbun shimbun-ryukyu)
+						   header)
+  (shimbun-ryukyu-prepare-article shimbun header))
+
+(provide 'sb-ryukyu)
+
+;;; sb-ryukyu.el ends here
---
+++ sb-taiwantoday2.el
@@ -0,0 +1,276 @@
+;;; sb-taiwantoday2.el --- shimbun backend for taiwantoday.tw -*- coding: iso-2022-7bit; -*-
+
+;; Modified from sb-yomiuri.el by lightcyan <lightcyan.area51@gmail.com>
+;; URL: http://d.hatena.ne.jp/lightcyan/
+;; Below is the original copyright notice of sb-reuters-rss.el.
+
+;; Author: http://pc.2ch.net/test/read.cgi/unix/1013710106/405-
+;; Keywords: news
+
+;;; Copyright:
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 2, or (at your option)
+;; any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program; if not, you can either send email to this
+;; program's maintainer or write to: The Free Software Foundation,
+;; Inc.; 59 Temple Place, Suite 330; Boston, MA 02111-1307, USA.
+
+;;; Commentary:
+
+;; Original code was sb-yomiuri.el(emacs-w3m-1.3.5) written by 
+;; TSUCHIYA Masatoshi <tsuchiya@namazu.org>,
+;; Yuuichi Teranishi  <teranisi@gohome.org>
+;; Katsumi Yamaoka    <yamaoka@jpl.org>
+
+;;; Code:
+
+(require 'shimbun)
+(require 'sb-text)
+
+(luna-define-class shimbun-taiwantoday2
+		   (shimbun-japanese-newspaper shimbun-text) ())
+
+(defvar shimbun-taiwantoday2-top-level-domain "taiwantoday.tw"
+  "Name of the top level domain for the Digital taiwantoday.")
+
+(defvar shimbun-taiwantoday2-url
+  (concat "http://" shimbun-taiwantoday2-top-level-domain)
+  "Name of the parent url.")
+
+(defvar shimbun-taiwantoday2-group-table
+  (let* ((s0 "[\t\n ]*")
+	 (s1 "[\t\n ]+")
+	 (default
+	   (list
+	    (concat
+	     "<a" s1 "href=\"" s0
+	     ;; 1. url 2. serial number
+	     "\\(ct\\.asp\\?xItem=\\([0-9]+\\)[^\"]+\\)\""
+	     "[^>]+>"
+	     ;; 3. subject
+	     "\\([^<]+\\)</a>"
+	     ;; 4. trash
+	     "\\(<[^>]+>\\)+"
+	     ;; 5. month
+	     "\\([0-1][0-9]\\)/"
+	     ;; 6. date
+	     "\\([0-3][0-9]\\)/"
+	     ;; 7. year
+	     "\\(20[0-9][0-9]\\)"
+	     )
+	    1 2 3 5 6 nil)))
+    `(("politics"     "政治"     "lp\.asp\?CtNode=1770\&CtUnit=146\&BaseDSD=12\&mp=1001" ,@default)
+      ("diplomacy"    "外交"     "lp\.asp\?CtNode=1771\&CtUnit=146\&BaseDSD=12\&mp=1001" ,@default)
+      ("economics"    "経済"     "lp\.asp\?CtNode=1772\&CtUnit=146\&BaseDSD=12\&mp=1001" ,@default)
+      ("culture"      "文化"     "lp\.asp\?CtNode=1773\&CtUnit=146\&BaseDSD=12\&mp=1001" ,@default)
+      ("sightseeing"  "観光"     "lp\.asp\?CtNode=1774\&CtUnit=146\&BaseDSD=12\&mp=1001" ,@default)
+      ("opinion"      "主張"     "lp\.asp\?CtNode=1776\&CtUnit=146\&BaseDSD=12\&mp=1001" ,@default)
+      ("critical"     "評論"     "lp\.asp\?CtNode=1777\&CtUnit=146\&BaseDSD=12\&mp=1001" ,@default)
+      ("politics2"    "特報政治" "lp\.asp\?CtNode=1892\&CtUnit=158\&BaseDSD=12\&mp=1001" ,@default)
+      ("diplomacy2"   "特報外交" "lp\.asp\?CtNode=1893\&CtUnit=158\&BaseDSD=12\&mp=1001" ,@default)
+      ("economics2"   "特報経済" "lp\.asp\?CtNode=1894\&CtUnit=158\&BaseDSD=12\&mp=1001" ,@default)
+      ("culture2"     "特報文化" "lp\.asp\?CtNode=1895\&CtUnit=158\&BaseDSD=12\&mp=1001" ,@default)
+      ("sightseeing2" "特報観光" "lp\.asp\?CtNode=1896\&CtUnit=158\&BaseDSD=12\&mp=1001" ,@default)
+      ))
+  "Alist of group names, their Japanese translations, index pages,
+regexps and numbers.
+Regexp may have the \"%s\" token which is replaced with a
+regexp-quoted group name.  Numbers point to the search result in order
+of a url, a serial number, a subject, a month, a day, an hour:minute
+and an extra keyword.")
+
+;; (defvar shimbun-taiwantoday2-content-start "\n[\t\n ]*<!!--bodystart-->\n")
+(defvar shimbun-taiwantoday2-content-start "<div[\t\n ]class=\"article\"[\t\n ]id=\"news_content\"><p>")
+;; (defvar shimbun-taiwantoday2-content-end  "\n[\t\n ]*<!!--bodyend-->\n")
+(defvar shimbun-taiwantoday2-content-end  "</div><!--#credit-->")
+(defvar shimbun-taiwantoday2-x-face-alist nil)
+
+(defvar shimbun-taiwantoday2-expiration-days 7)
+
+(luna-define-method initialize-instance :after ((shimbun shimbun-taiwantoday2)
+						 &rest init-args)
+  (shimbun-set-server-name-internal shimbun "台湾")
+  (shimbun-set-from-address-internal shimbun
+				     (concat "webmaster@www."
+					     shimbun-taiwantoday2-top-level-domain))
+  ;; To share class variables between `shimbun-taiwantoday2' and its
+  ;; successor `shimbun-taiwantoday2-html'.
+  (shimbun-set-x-face-alist-internal shimbun shimbun-taiwantoday2-x-face-alist)
+  (shimbun-set-expiration-days-internal shimbun
+					shimbun-taiwantoday2-expiration-days)
+  shimbun)
+
+(luna-define-method shimbun-groups ((shimbun shimbun-taiwantoday2))
+  (mapcar 'car shimbun-taiwantoday2-group-table))
+
+(luna-define-method shimbun-current-group-name ((shimbun shimbun-taiwantoday2))
+  (nth 1 (assoc (shimbun-current-group-internal shimbun)
+		shimbun-taiwantoday2-group-table)))
+
+(luna-define-method shimbun-index-url ((shimbun shimbun-taiwantoday2))
+  (let ((group (shimbun-current-group-internal shimbun)))
+    (concat shimbun-taiwantoday2-url "/"
+	    (nth 2 (assoc group shimbun-taiwantoday2-group-table)))))
+
+(defmacro shimbun-taiwantoday2-japanese-string-to-number (string)
+  "Convert a Japanese zenkaku number to just a number."
+  (let ((alist ''((?０ . 0) (?１ . 1) (?２ . 2) (?３ . 3) (?４ . 4)
+		  (?５ . 5) (?６ . 6) (?７ . 7) (?８ . 8) (?９ . 9))))
+    (if (= (length "０") 1)
+	`(let* ((str ,string)
+		(alist ,alist)
+		(len (length str))
+		(idx 0)
+		(num 0))
+	   (while (< idx len)
+	     (setq num (+ (cdr (assq (aref str idx) alist)) (* num 10))
+		   idx (1+ idx)))
+	   num)
+      `(let* ((str ,string)
+	      (alist ,alist)
+	      (len (length str))
+	      (idx 0)
+	      (num 0)
+	      char)
+	 (while (< idx len)
+	   (setq char (sref str idx)
+		 num (+ (cdr (assq char alist)) (* num 10))
+		 idx (+ idx (char-bytes char))))
+	 num))))
+
+(defun shimbun-taiwantoday2-shorten-brackets-in-string (string)
+  "Replace Japanes zenkaku brackets with ascii characters in STRING.
+It does also shorten too much spaces."
+  (with-temp-buffer
+    (insert string)
+    (let ((alist '(("（" . " (") ("）" . ") ") ("［" . " [")
+		   ("］" . "] ") ("｛" . " {") ("｝" . "} ")))
+	  elem)
+      (while alist
+	(setq elem (pop alist))
+	(goto-char (point-min))
+	(while (search-forward (car elem) nil t)
+	  (replace-match (cdr elem))))
+      (goto-char (point-min))
+      (while (re-search-forward "[\t 　]+" nil t)
+	(replace-match " "))
+      (goto-char (point-min))
+      (while (re-search-forward "\\([])}]\\) \\([])}]\\)" nil t)
+	(replace-match "\\1\\2")
+	(forward-char -1))
+      (goto-char (point-min))
+      (while (re-search-forward "\\([[({]\\) \\([[({]\\)" nil t)
+	(replace-match "\\1\\2")
+	(forward-char -1))
+      (goto-char (point-min))
+      (while (re-search-forward " ?\\([「」]\\) ?" nil t)
+	(replace-match "\\1"))
+      (goto-char (point-min))
+      (while (re-search-forward "^ \\| $" nil t)
+	(replace-match "")))
+    (buffer-string)))
+
+(defun shimbun-taiwantoday2-get-headers (shimbun)
+  "Return a list of headers."
+  (let ((group (shimbun-current-group-internal shimbun))
+	(from (shimbun-from-address shimbun))
+	(case-fold-search t)
+	regexp numbers cyear cmonth month year day headers)
+    (setq regexp (assoc group shimbun-taiwantoday2-group-table)
+	  numbers (nthcdr 4 regexp)
+	  regexp (format (nth 3 regexp) (regexp-quote group))
+	  cyear (decode-time)
+	  cmonth (nth 4 cyear)
+	  cyear (nth 5 cyear))
+    (while (re-search-forward regexp nil t)
+      (setq month (cond ((nth 3 numbers)
+			 (string-to-number (match-string (nth 3 numbers))))
+			((nth 6 numbers)
+			 (shimbun-taiwantoday2-japanese-string-to-number
+			  (match-string (nth 6 numbers))))
+			(t
+			 1))
+	    year (cond ((and (= 12 month) (= 1 cmonth))
+			(1- cyear))
+		       ((and (= 1 month) (= 12 cmonth))
+			(1+ cyear))
+		       (t
+			cyear))
+	    day (cond ((nth 4 numbers)
+		       (string-to-number (match-string (nth 4 numbers))))
+		      ((nth 7 numbers)
+		       (shimbun-taiwantoday2-japanese-string-to-number
+			(match-string (nth 7 numbers))))
+		      (t
+		       1)))
+      (push (shimbun-make-header
+	     ;; number
+	     0
+	     ;; subject
+	     (if (and (nth 6 numbers) (nth 7 numbers))
+		 (shimbun-mime-encode-string
+		  (format "%02d/%02d %s"
+			  month day
+			  (shimbun-taiwantoday2-shorten-brackets-in-string
+			   (match-string (nth 2 numbers)))))
+	       (shimbun-mime-encode-string (match-string (nth 2 numbers))))
+	     ;; from
+	     from
+	     ;; date
+	     (shimbun-make-date-string year month day
+				       (when (nth 5 numbers)
+					 (match-string (nth 5 numbers))))
+	     ;; id
+	     (format "<%d%02d%02d.%s%%%s.%s>"
+		     year month day (match-string (nth 1 numbers)) group
+		     shimbun-taiwantoday2-top-level-domain)
+	     ;; references, chars, lines
+	     "" 0 0
+	     ;; xref
+	     (concat shimbun-taiwantoday2-url "/" (match-string (nth 0 numbers))))
+	    headers))
+    headers))
+
+(luna-define-method shimbun-get-headers ((shimbun shimbun-taiwantoday2)
+					 &optional range)
+  (shimbun-taiwantoday2-get-headers shimbun))
+
+(defun shimbun-taiwantoday2-adjust-date-header (shimbun header)
+  "Adjust a date header if there is a correct information available."
+  (let ((case-fold-search t)
+	end)
+    (when (and (re-search-forward (shimbun-content-start-internal shimbun)
+				  nil t)
+	       (re-search-forward (shimbun-content-end-internal shimbun)
+				  nil t)
+	       (progn
+		 (setq end (match-beginning 0))
+		 (beginning-of-buffer)
+		 (re-search-forward "\\(20[0-9][0-9]\\)\.\\([01][0-9]\\)\.\
+\\([0123][0-9]\\).+\\([012][0-9]:[0-5][0-9]\\)"
+				    end t)))
+      (shimbun-header-set-date
+       header
+       (shimbun-make-date-string
+	(string-to-number (match-string 1))
+	(string-to-number (match-string 2))
+	(string-to-number (match-string 3))
+	(match-string 4)))))
+  (goto-char (point-min)))
+
+(luna-define-method shimbun-make-contents :before ((shimbun shimbun-taiwantoday2)
+						   header)
+  (shimbun-taiwantoday2-adjust-date-header shimbun header))
+
+(provide 'sb-taiwantoday2)
+
+;;; sb-taiwantoday2.el ends here
